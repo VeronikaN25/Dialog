610e8f82fa900c7d1175c63fe74be03ac80989a7
Veeя <novichikhina.05@mail.ru>
2024-03-08 22:03:11 +0600

    Finish Alert Dialog

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.myapplication
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showAlertDialog()
+    }
+    private fun showAlertDialog(){
+        val alert_dialog = resources.getDrawable(R.drawable.alert_dialog)
+        val builder = AlertDialog.Builder(this)
+        val dialog=builder
+           .setView(R.layout.alert_dialog)
+           .setPositiveButton("Confirm"){dialog, _ ->
+               dialog.cancel()
+           }
+           .setNegativeButton("Dismiss") { dialog, _ ->
+               dialog.cancel()
+           }
+           .create()
+        dialog.window?.setBackgroundDrawable(alert_dialog)
+        dialog.show()
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/ShowAlertDialog.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import android.app.DialogFragment
+import android.os.Bundle
+import android.view.View
+
+class ShowAlertDialog:DialogFragment() {
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyOptionsMenu() {
+        super.onDestroyOptionsMenu()
+    }
+}
