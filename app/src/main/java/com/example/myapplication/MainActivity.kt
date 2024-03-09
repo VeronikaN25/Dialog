d31a1e8d50bbc6829afcbadf374c8645e90067bf
Veeя <novichikhina.05@mail.ru>
2024-03-09 15:37:41 +0600

    Finish Dialog Fragment

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,16 +2,25 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
-class MainActivity : AppCompatActivity() {
+const val TAG_FRAGMENT = "Tag_Dialog_fragment"
+
+class MainActivity : AppCompatActivity(), OnClickFragment {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        showAlertDialog()
+
+        val fragmentDialog = ShowAlertDialog.newInstance(
+            label = "This text for Alert Dialog",
+            image = R.drawable.dialog
+        )
+        fragmentDialog.show(supportFragmentManager, TAG_FRAGMENT)
+//        showAlertDialog()
     }
     private fun showAlertDialog(){
-        val alert_dialog = resources.getDrawable(R.drawable.alert_dialog)
+        val alertDialog = resources.getDrawable(R.drawable.alert_dialog)
         val builder = AlertDialog.Builder(this)
         val dialog=builder
            .setView(R.layout.alert_dialog)
@@ -22,7 +31,15 @@ class MainActivity : AppCompatActivity() {
                dialog.cancel()
            }
            .create()
-        dialog.window?.setBackgroundDrawable(alert_dialog)
+        dialog.window?.setBackgroundDrawable(alertDialog)
         dialog.show()
+    }
+
+    override fun clickOk() {
+        Toast.makeText(this,"Press ok",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun clickCancel() {
+        Toast.makeText(this,"Press cancel",Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/example/myapplication/OnClickFragment.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication
+
+interface OnClickFragment {
+        fun clickOk()
+        fun clickCancel()
+}
